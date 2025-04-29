0075f5cdff501b78507dc465301ff4ee52712728
shibilabasheer <shibila.abasheer@gmail.com>
2025-04-29 19:40:41 +0530

    variable type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 document.getElementById("addTask").addEventListener('click', () => {
 
-    var taskInput = document.getElementById("taskInput").value;
+    const taskInput = document.getElementById("taskInput").value;
 
     if (taskInput.trim() == "") {
         alert("Please enter a task.");
@@ -9,16 +9,16 @@ document.getElementById("addTask").addEventListener('click', () => {
     }
 
     //create a li element
-    var li = document.createElement("li");
+    const li = document.createElement("li");
     li.className = "list-group-item d-flex justify-content-between align-items-center list-group-item-success";
 
     //create a span with content as the task added and append to li
-    var span = document.createElement("span");
+    const span = document.createElement("span");
     span.textContent = taskInput;
     li.appendChild(span);
 
     //create a delete button and append to li
-    var deleteButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("delete"));
     deleteButton.className = "btn btn-sm btn-danger";
     li.appendChild(deleteButton);
